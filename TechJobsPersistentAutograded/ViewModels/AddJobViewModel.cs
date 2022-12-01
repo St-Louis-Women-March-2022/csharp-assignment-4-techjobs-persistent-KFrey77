b2e3629ae01859ccc695fa52aae018909b0ef9a4
Zombie <kroshel77@gmail.com>
2022-11-30 20:42:03 -0600

    Part 2.

---
+++ TechJobsPersistentAutograded/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using TechJobsPersistentAutograded.Models;
+
+namespace TechJobsPersistentAutograded.ViewModels
+{
+    public class AddJobViewModel
+    {
+        
+        public string Name { get; set; }
+        public int EmployerId { get; set; }
+
+        public List<SelectListItem> Employers { get; set; }
+
+
+        public AddJobViewModel(List<Employer> employers)
+        {
+            Employers = new List<SelectListItem>();
+
+            foreach (var employer in employers)
+            {
+                Employers.Add(new SelectListItem
+                {
+                    Value = employer.Id.ToString(),
+                    Text = employer.Name
+                });
+            }
+        }
+    }
+
+        }
+}
